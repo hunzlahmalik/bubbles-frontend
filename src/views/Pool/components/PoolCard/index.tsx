14d3dc9bec01e37d76bfbd2e31c33c093d6d06ce
Ash_Ch <ashch1215@gmail.com>
2021-12-31 17:12:39 +0500

    added: Pool cards

---
+++ src/views/Pool/components/PoolCard/index.tsx
@@ -0,0 +1,249 @@
+import React, { useMemo } from 'react';
+import styled from 'styled-components';
+import { Pair } from '@pancakeswap/sdk';
+import { Text, CardBody, CardFooter, Button } from 'bubbles-uikit';
+import { Link } from 'react-router-dom';
+import { useTranslation } from 'contexts/Localization';
+import useActiveWeb3React from 'hooks/useActiveWeb3React';
+import FullPositionCard from '../../../../components/PositionCard';
+import { useTokenBalancesWithLoadingIndicator } from '../../../../state/wallet/hooks';
+import { usePairs } from '../../../../hooks/usePairs';
+import { toV2LiquidityToken, useTrackedTokenPairs } from '../../../../state/user/hooks';
+import Dots from '../../../../components/Loader/Dots';
+import { AppBody } from '../../../../components/App';
+
+const PageCardDesign = styled.div`
+  margin-top: 10px;
+  margin: auto;
+  margin-bottom: 10px;
+  width: calc(50%-30px);
+  min-width: 300px;
+  padding: 0;
+  font-size: 16px;
+  line-height: 1.4;
+  color: inherit;
+  background: transparent;
+  :hover {
+    filter: brightness(95%);
+  }
+`;
+const BoxHeader = styled.div`
+  position: relative;
+  padding-bottom: 100%;
+  height: 0;
+  border-top-right-radius: 16px;
+  border-top-left-radius: 16px;
+  cursor: pointer;
+  overflow: hidden;
+  border-top-right-radius: 16px;
+  border-top-left-radius: 16px;
+`;
+// const SubBoxHeader1=styled.div`
+//   background-image:url(https://jojo.fun/img/add.e4b25d9e.svg);
+//   background-color:rgb(249,244,211);
+//   filter: blur(0px);
+//   background-size: contain;
+//   background-position-x: 40%;
+//   background-position-y: 50%;
+//   position: absolute;
+//   top: 0;
+//   left: 0;
+//   width: 100%;
+//   height: 100%;
+//   border-radius:20px;
+// `
+const SubBoxHeader2 = styled.div`
+  position: absolute;
+  top: 15px;
+  right: 15px;
+  display: inline-flex;
+  transition-property: opacity;
+  transition-duration: 0.2s;
+  transition-timing-function: ease;
+  transition-delay: 0s;
+  opacity: 0;
+`;
+
+const BoxHeaderTitle = styled.div`
+  padding-top: 14px;
+  padding-right: 24px;
+  padding-bottom: 3px;
+  padding-left: 24px;
+  background-color: #fff;
+  height: 36px;
+  align-items: center;
+  display: flex;
+  font-size: 18px;
+  line-height: 1.2;
+  color: inherit;
+  word-break: break-word;
+  font-weight: 700;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+`;
+
+const BoxLine = styled.div`
+  height: 30px;
+  min-width: unset;
+  width: calc(100% - 26px);
+  background-image: url(https://jojo.fun/img/bg-line.fea0371a.svg);
+  background-repeat: repeat-x;
+  background-size: auto 1px;
+  margin: 0px auto;
+  background-color: #fff;
+  position: relative;
+  background-position-x: 50%;
+  background-position-y: center;
+`;
+
+const Body = styled(CardBody)`
+  // background-color: ${({ theme }) => theme.colors.dropdownDeep};
+  padding-top: 5px;
+  padding-right: 24px;
+  padding-bottom: 20px;
+  padding-left: 24px;
+  min-width: 250px;
+  max-width: 300px;
+  flex-grow: 1;
+  flex-shrink: 1;
+  flex-basis: 0%;
+`;
+
+const BodyTitle = styled.div`
+  display: flex;
+  -webkit-box-pack: justify;
+  justify-content: space-between;
+  font-size: 14px;
+  color: inherit;
+`;
+export default function PoolCard(title, BackImg) {
+  const { account } = useActiveWeb3React();
+  const { t } = useTranslation();
+
+  // fetch the user's balances of all tracked V2 LP tokens
+  const trackedTokenPairs = useTrackedTokenPairs();
+  const tokenPairsWithLiquidityTokens = useMemo(
+    () => trackedTokenPairs.map((tokens) => ({ liquidityToken: toV2LiquidityToken(tokens), tokens })),
+    [trackedTokenPairs],
+  );
+  const liquidityTokens = useMemo(
+    () => tokenPairsWithLiquidityTokens.map((tpwlt) => tpwlt.liquidityToken),
+    [tokenPairsWithLiquidityTokens],
+  );
+  const [v2PairsBalances, fetchingV2PairBalances] = useTokenBalancesWithLoadingIndicator(
+    account ?? undefined,
+    liquidityTokens,
+  );
+
+  // fetch the reserves for all V2 pools in which the user has a balance
+  const liquidityTokensWithBalances = useMemo(
+    () =>
+      tokenPairsWithLiquidityTokens.filter(({ liquidityToken }) =>
+        v2PairsBalances[liquidityToken.address]?.greaterThan('0'),
+      ),
+    [tokenPairsWithLiquidityTokens, v2PairsBalances],
+  );
+
+  const v2Pairs = usePairs(liquidityTokensWithBalances.map(({ tokens }) => tokens));
+  const v2IsLoading =
+    fetchingV2PairBalances ||
+    v2Pairs?.length < liquidityTokensWithBalances.length ||
+    v2Pairs?.some((V2Pair) => !V2Pair);
+
+  const allV2PairsWithLiquidity = v2Pairs.map(([, pair]) => pair).filter((v2Pair): v2Pair is Pair => Boolean(v2Pair));
+
+  const renderBody = () => {
+    if (!account) {
+      return (
+        <>
+          <Text color="textSubtle" textAlign="center">
+            {t('Position HashRate')}
+          </Text>
+          <Text textAlign="center">{t('1')}</Text>
+        </>
+      );
+    }
+    if (v2IsLoading) {
+      return (
+        <Text color="textSubtle" textAlign="center">
+          <Dots>{t('Loading')}</Dots>
+        </Text>
+      );
+    }
+    // dont understand this rn
+    if (allV2PairsWithLiquidity?.length > 0) {
+      return allV2PairsWithLiquidity.map((v2Pair, index) => (
+        <FullPositionCard
+          key={v2Pair.liquidityToken.address}
+          pair={v2Pair}
+          mb={index < allV2PairsWithLiquidity.length - 1 ? '16px' : 0}
+        />
+      ));
+    }
+    return (
+      <>
+        <Text color="textSubtle" textAlign="center">
+          {t('Position HashRate')}
+        </Text>
+        <Text textAlign="center">{t('1')}</Text>
+      </>
+    );
+  };
+
+  return (
+    <>
+      <PageCardDesign>
+        <AppBody>
+          <BoxHeader>
+            <img
+              src={BackImg}
+              style={{
+                backgroundColor: 'rgb(249,244,211)',
+                filter: 'blur(0px)',
+                backgroundSize: 'contain',
+                backgroundPositionX: '40%',
+                backgroundPositionY: '50%',
+                position: 'absolute',
+                top: '0',
+                left: '0',
+                width: '100%',
+                height: '100%',
+                borderRadius: '20px',
+              }}
+              alt="nothing"
+            />
+
+            <SubBoxHeader2 />
+          </BoxHeader>
+          <BoxHeaderTitle>
+            {title}
+            {/* 
+                    <AppHeader title={t('Your Liquidity')} subtitle={t('Remove liquidity to receive tokens back')} /> */}
+          </BoxHeaderTitle>
+          <BoxLine />
+
+          <Body>
+            <BodyTitle>
+              {renderBody()}
+              {account && !v2IsLoading && (
+                <Text color="textSubtle" mb="8px">
+                  {t("Don't see a pool you joined?")}
+                </Text>
+              )}
+            </BodyTitle>
+            <CardFooter style={{ textAlign: 'center' }}>
+              <Button style={{ marginTop: '10px' }} id="join-pool-button" as={Link} to="/add" width="100%">
+                {t('Stake')}
+              </Button>
+              <Button style={{ marginTop: '10px' }} id="join-pool-button" as={Link} to="/add" width="100%">
+                {t('upgrade hashrate')}
+              </Button>
+            </CardFooter>
+          </Body>
+        </AppBody>
+      </PageCardDesign>
+    </>
+  );
+}
--- src/views/Pool/index.tsx
+++ src/views/Pool/index.tsx
@@ -3,7 +3,50 @@ import Page from 'components/Layout/Page';
 import TabButtonMenu, { TabButtonMenuProps } from 'components/TabButtonMenu';
 import ImageCard, { ImageCardProps } from 'components/ImageCard';
 import GradientCard from 'components/GradientCard';
+import styled from 'styled-components';
 import GamePool from './components/GamePool';
+import PoolCard from './components/PoolCard';
+
+const CardBox = styled.div`
+  margin-top: 6px;
+  display: flex;
+  flex-wrap: wrap;
+  margin-bottom: 6px;
+  padding-top: 32px;
+  padding-right: 0px;
+  padding-bottom: 32px;
+  padding-left: 0px;
+  padding: 32px;
+
+  margin: auto;
+  margin-top: 48px;
+  position: relative;
+  background-size: cover;
+  display: block;
+  position: relative;
+  box-sizing: border-box;
+  background-color: transparent;
+`;
+const CardDesign = styled.div`
+  width: calc(70%-30px);
+  background-color: transparent;
+  display: flex;
+  flex-wrap: wrap;
+  -webkit-boc-flex: 1;
+  flex-grow: 1;
+  flex-shrink: 1;
+  flex-basis: 0%;
+  margin-top: 30px;
+  margin-right: auto;
+  margin-bottom: 10px;
+  margin-left: auto;
+  box-size: inherit;
+  border-radius: 16px;
+  overflow: hidden;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  box-shadow: unset;
+`;
 
 const gamepooldata = {
   large: [
@@ -119,6 +162,16 @@ const Pool: React.FC = () => {
   const menu2 = menu.map((ele) => ele.subMenu);
 
   console.log(menu2);
+  const data = [
+    { id: 1, title: 'Add Nft', img: 'https://jojo.fun/img/add.e4b25d9e.svg' },
+    { id: 2, title: 'Add Nft', img: 'https://jojo.fun/img/add.e4b25d9e.svg' },
+    { id: 3, title: 'Add Nft', img: 'https://jojo.fun/img/add.e4b25d9e.svg' },
+    { id: 4, title: 'Add Nft', img: 'https://jojo.fun/img/add.e4b25d9e.svg' },
+    { id: 5, title: 'Add Nft', img: 'https://jojo.fun/img/add.e4b25d9e.svg' },
+    { id: 6, title: 'Add Nft', img: 'https://jojo.fun/img/add.e4b25d9e.svg' },
+    { id: 7, title: 'Add Nft', img: 'https://jojo.fun/img/add.e4b25d9e.svg' },
+    { id: 8, title: 'Add Nft', img: 'https://jojo.fun/img/add.e4b25d9e.svg' },
+  ];
 
   return (
     <>
@@ -131,6 +184,13 @@ const Pool: React.FC = () => {
       <br />
       <br />
       <GamePool />
+      <CardBox>
+        <CardDesign>
+          {data.map((cardT) => (
+            <>{PoolCard(cardT.title, cardT.img)}</>
+          ))}
+        </CardDesign>
+      </CardBox>
     </>
   );
 };
